ac04c9ae32379081982ba22ff1d39790e8f0b69e
Mariam Ait Al <Aital_1998@outlook.com>
2018-11-20 13:15:51 +0000

    discover

--- src/UserBundle/Controller/DiscoverController.php
+++ src/UserBundle/Controller/DiscoverController.php
@@ -397,11 +397,64 @@ class DiscoverController extends Controller
         if(!is_null($pan))
         {
             $session = new Session();
+            // get pan average rating
+            $ratingrepo = $this->getDoctrine()->getRepository('UserBundle:PanRating');
+            $rating = $ratingrepo->findBy(
+                array('pan'=>$pan)
+            );
+            $average = 0;
+            if(sizeof($rating)>0)
+            {
+                foreach ($rating as $val)
+                {
+                    $average += $val->getRate();
+                }
+                $average = $average / sizeof($rating);
+                if($average > 5)
+                    $average = 5;
+            }
+            // get product reviews
+            $userrepo = $this->getDoctrine()->getRepository('AppBundle:User');
+            $prrepo = $this->getDoctrine()->getRepository('UserBundle:PanReview');
+            $pr = $prrepo->findBy(
+                array('pan'=>$pan)
+            );
+            $reviews = array();
+            if(!empty($pr))
+            {
+                foreach ($pr as $rev)
+                {
+                    //Get review owner
+                    $reviewOwner = $userrepo->findOneById($rev->getUser());
+                    //Get owner rating
+                    $ownerRate = $ratingrepo->findOneBy(
+                        array('user'=>$reviewOwner,'pan'=>$pan)
+                    );
+                    if(!is_null($ownerRate)) $rate = $ownerRate->getRate(); else $rate = 'no rating';
+                    //
+                    $rtemp = array(
+                        'review_owner_id'=>$reviewOwner->getId(),
+                        'review_owner_first_name'=>$reviewOwner->getFirstname(),
+                        'review_owner_last_name'=>$reviewOwner->getLastname(),
+                        'review_owner_pan_rating'=>$rate,
+                        'review_owner_image'=>$reviewOwner->getImage(),
+                        'review_owner_content'=>$rev->getReview()
+                    );
+                    array_push($reviews,$rtemp);
+                }
+            }
+            //
             $temp = array(
-                'product'=>$pan
+                'product'=>$pan,
+                'average'=>$average,
+                'reviews'=>$reviews
             );
             $session->set('selected_product',$temp);
             return $this->render('UserBundle::product.html.twig');
+        }
+        else
+        {
+
         }
     }
 }
