0836d87c84c8b83cafe43633485b97c689a39055
Mariam Ait Al <Aital_1998@outlook.com>
2018-11-21 00:42:15 +0000

    groups view

---
+++ src/UserBundle/Controller/GroupController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace UserBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Entity\Groupe;
+
+class GroupController extends Controller
+{
+    /**
+     * @Route("/linkpan/groups",name="groups")
+     */
+    public function groupsAction()
+    {
+        return $this->render('UserBundle::groups.html.twig');
+    }
+
+    /**
+     * @Route("/linkpan/new_group",name="new_group")
+     */
+    public function newgroupAction(Request $request)
+    {
+        $currentUser = $this->getUser();
+        if(!is_null($request->files->get('groupimage')))
+        {
+           /** $group = new Groupe();
+            $group->setUser($currentUser);
+            // upload  image
+            $attdir = __DIR__ . "/../../../web/images/group";
+            $image = $request->files->get('groupimage');
+            $files = scandir($attdir);
+            $count = count($files)-2;
+            $name = $count.'_'.$image->getClientOriginalName();
+            $image->move($attdir,$name );
+            //
+            $group->setImage($name);**/
+        }
+    }
+}
--- src/UserBundle/Controller/PanController.php
+++ src/UserBundle/Controller/PanController.php
@@ -7,7 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use UserBundle\Entity\Pan;
-use DateTime;
+
 use UserBundle\Entity\PanReview;
 use UserBundle\Entity\PanShare;
 
