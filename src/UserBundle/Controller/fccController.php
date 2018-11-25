fa9f93e26f0d3dc0855ff246052307c0d99d1f78
Mariam Ait Al <Aital_1998@outlook.com>
2018-11-25 02:18:17 +0000

    complaint item

--- src/UserBundle/Controller/fccController.php
+++ src/UserBundle/Controller/fccController.php
@@ -6,14 +6,95 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
+use UserBundle\Entity\Complaint;
 
 class fccController extends Controller
 {
+
+    /**
+     * @Route("{_locale}/linkpan/complaints",name="complaints")
+     */
+    public function complaintsAction()
+    {
+        return $this->render('UserBundle::complaints.html.twig');
+    }
+
+
     /**
      * @Route("{_locale}/linkpan/complaints/add_complaint",name="add_complaint")
      */
     public function add_complaintAction()
     {
+        $data = array();
+        $currentUser = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT u
+            FROM AppBundle:User u
+            WHERE u.id != :user
+            ORDER BY u.lastname'
+        )->setParameter('user', $currentUser->getId());
+        if(!is_null($query))
+        {
+            $users = $query->getResult();
+            foreach ($users as $item)
+            {
+                $temp = array(
+                    'id'=>$item->getId(),
+                    'firstname'=>$item->getFirstname(),
+                    'lastname'=>$item->getLastname()
+                );
+                array_push($data,$temp);
+            }
+        }
+        $session = new Session();
+        $session->set('all_users',$data);
         return $this->render('UserBundle::complaintForm.html.twig');
+    }
+
+
+
+    /**
+     * @Route("{_locale}/linkpan/complaints/save_complaint",name="save_complaint")
+     */
+    public function savecomplaintAction(Request $request)
+    {
+        $repo = $this->getDoctrine()->getRepository('AppBundle:User');
+        $userToComplaint = $repo->findOneById($request->get('userlist'));
+        $frepo = $this->getDoctrine()->getRepository('UserBundle:Complaint');
+        $fcc = $frepo->findOneBy(
+          array('user'=>$this->getUser(),'userToComplaint'=>$userToComplaint)
+        );
+        if(!is_null($fcc))
+            echo '<script language="javascript">alert("You cannot complaint same user twice , if there s any addition can u please update the last complaint you publish about this user , Thanks")</script>';
+        else
+        {
+            $complaint = new Complaint();
+            if(!is_null($userToComplaint))
+                $complaint->setUserToComplaint($userToComplaint);
+
+            $complaint->setTitle($request->get('title'));
+            $complaint->setCause($request->get('cause'));
+            $complaint->setTransaction($request->get('amount'));
+            if (!is_null($request->files->get('file')))
+            {
+                $dir = __DIR__ . "/../../../web/attachements/fcc";
+                $file = $request->files->get('file');
+                $files = scandir($dir);
+                $count = count($files)-2;
+                $name = $count.'_'.$file->getClientOriginalName();
+                $file->move($dir,$name );
+                $complaint->setFile($name);
+            }
+            $complaint->setDescription($request->get('description'));
+            $complaint->setUser($this->getUser());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($complaint);
+            $em->flush();
+
+            echo '<script language="javascript">alert("Complaint Saved ! Thank you")</script>';
+        }
+
+        return $this->forward('UserBundle:fcc:complaints');
     }
 }
---
+++ src/UserBundle/Entity/Complaint.php
@@ -0,0 +1,237 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Complaint
+ *
+ * @ORM\Table(name="complaint")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\ComplaintRepository")
+ */
+class Complaint
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="text")
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cause", type="text")
+     */
+    private $cause;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="transaction", type="text")
+     */
+    private $transaction;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="file", type="text")
+     */
+    private $file;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $userToComplaint;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Complaint
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set cause
+     *
+     * @param string $cause
+     *
+     * @return Complaint
+     */
+    public function setCause($cause)
+    {
+        $this->cause = $cause;
+    
+        return $this;
+    }
+
+    /**
+     * Get cause
+     *
+     * @return string
+     */
+    public function getCause()
+    {
+        return $this->cause;
+    }
+
+    /**
+     * Set transaction
+     *
+     * @param string $transaction
+     *
+     * @return Complaint
+     */
+    public function setTransaction($transaction)
+    {
+        $this->transaction = $transaction;
+    
+        return $this;
+    }
+
+    /**
+     * Get transaction
+     *
+     * @return string
+     */
+    public function getTransaction()
+    {
+        return $this->transaction;
+    }
+
+    /**
+     * Set file
+     *
+     * @param string $file
+     *
+     * @return Complaint
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+    
+        return $this;
+    }
+
+    /**
+     * Get file
+     *
+     * @return string
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Complaint
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserToComplaint()
+    {
+        return $this->userToComplaint;
+    }
+
+    /**
+     * @param mixed $userToComplaint
+     */
+    public function setUserToComplaint($userToComplaint)
+    {
+        $this->userToComplaint = $userToComplaint;
+    }
+
+
+}
+
