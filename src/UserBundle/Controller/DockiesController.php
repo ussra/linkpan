e98c6d6ff7ecdcb8b2d8b32766262a7252c47930
Mariam Ait Al <Aital_1998@outlook.com>
2018-11-25 21:14:04 +0000

    2511_lastupdate

---
+++ src/UserBundle/Controller/DockiesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+class DockiesController extends Controller
+{
+
+}
